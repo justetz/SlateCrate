82c3ca8543043cb81087d718a804d6ff4baaf940
Justin Etzine <justetz@gmail.com>
2015-12-06 17:07:44 -0500

    JS comments, removed unused file

--- assets/js/listings.js
+++ assets/js/listings.js
@@ -1,5 +1,17 @@
+/**
+ * This file is used by classes.php and links.php, as they have similar structures and matching IDs and classes used
+ * below.
+ */
+
+/**
+ * Enables a tooltip that appears above the search box when clicked on that explains how to submit the search field
+ */
 $('#search').tooltip({'trigger':'focus', 'title': 'Hit ENTER to search'});
 
+/**
+ * Requires the user to confirm deletion before submitting the item for deletion. If the user declines, the item is not
+ * deleted.
+ */
 $(".delete-form").submit(function() {
     return confirm('Are you sure you want to delete this item?');
 });
