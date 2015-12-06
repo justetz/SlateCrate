6e0462646bf5a19ba4867777398c7c5e3b3d7c86
Justin Etzine <justetz@gmail.com>
2015-12-06 17:04:04 -0500

    populateData comments

--- resources/classFunctions.php
+++ resources/classFunctions.php
@@ -9,7 +9,8 @@
  * @return array string containing whether a success or error alert should be displayed and the message to display in
  *               the alert
  */
-function executeEdit($conn, $newTitle, $newPrefix, $id) {
+function executeEdit($conn, $newTitle, $newPrefix, $id)
+{
     try {
         $conn->query("UPDATE `categories` SET `title` = '" . $newTitle . "', `prefix` = '" . $newPrefix . "' WHERE `category_id` = " . $id);
 
@@ -29,7 +30,8 @@ function executeEdit($conn, $newTitle, $newPrefix, $id) {
  * @return array string containing whether a success or error alert should be displayed and the message to display in
  *               the alert
  */
-function executeAdd($conn, $className, $inputCategory, $user) {
+function executeAdd($conn, $className, $inputCategory, $user)
+{
     try {
         $string = "'" . $className . "', '" . $inputCategory . "', '" . $user . "', " . "CURDATE()";
 
@@ -49,17 +51,18 @@ function executeAdd($conn, $className, $inputCategory, $user) {
  * @return array string containing whether a success or error alert should be displayed and the message to display in
  *               the alert
  */
-function executeDelete($conn, $categoryId) {
+function executeDelete($conn, $categoryId)
+{
     try {
         $del = $conn->prepare("SELECT `link_id` FROM `links` WHERE `category_id` = " . $categoryId);
         $del->execute();
-        while($result = $del->fetch(PDO::FETCH_ASSOC)){
+        while ($result = $del->fetch(PDO::FETCH_ASSOC)) {
             $conn->query("DELETE FROM `links` WHERE `link_id` = " . $result["link_id"]);
         }
         $conn->query("DELETE FROM `categories` WHERE `category_id` = " . $categoryId);
 
-        return array("success","The class, and its links, have been deleted!");
-    } catch(PDOException $e){
+        return array("success", "The class, and its links, have been deleted!");
+    } catch (PDOException $e) {
         return array("error", $e);
     }
 }
@@ -71,9 +74,10 @@ function executeDelete($conn, $categoryId) {
  * @param $currentPrefix string the current prefix in the '?class=' portion of the URI. If a class is not selected, an
  *                       empty string should be passed in.
  */
-function populateSidebar($currentPrefix) {
+function populateSidebar($currentPrefix)
+{
     echo "<li role='presentation'";
-    if($currentPrefix == "") {
+    if ($currentPrefix == "") {
         echo " class='active'";
     }
     echo "><a href='classes.php'>All Prefixes</a></li>";
@@ -96,8 +100,9 @@ function populateSidebar($currentPrefix) {
  * @param $prefix string the currently selected prefix, empty string if none.
  * @param $currentPage int the currently visible page
  */
-function populatePagination($count, $prefix, $currentPage) {
-    if(($count / 19) + 1 >= 2) {
+function populatePagination($count, $prefix, $currentPage)
+{
+    if (($count / 19) + 1 >= 2) {
         echo "<div class=\"col-xs-12 centered\"><hr/><div class=\"btn-group\">";
 
         $link = "?";
@@ -106,24 +111,24 @@ function populatePagination($count, $prefix, $currentPage) {
         }
 
         echo "<a ";
-        if($currentPage > 1) {
-            echo "href=\"".$link."page=".($currentPage-1)."\"";
+        if ($currentPage > 1) {
+            echo "href=\"" . $link . "page=" . ($currentPage - 1) . "\"";
         } else {
             echo "disabled";
         }
         echo " class=\"btn btn-primary\"><span class='fa fa-chevron-left'></span></a>";
 
         for ($button = 1; $button < ($count / 18) + 1; $button++) {
-            echo "<a href=\"".$link."page=$button\" class=\"btn btn-primary";
-            if(intval($button) == intval($currentPage)) {
+            echo "<a href=\"" . $link . "page=$button\" class=\"btn btn-primary";
+            if (intval($button) == intval($currentPage)) {
                 echo " active";
             }
             echo "\">$button</a>";
         }
 
         echo "<a ";
-        if($currentPage < ($count / 18)) {
-            echo "href=\"".$link."page=".($currentPage+1)."\"";
+        if ($currentPage < ($count / 18)) {
+            echo "href=\"" . $link . "page=" . ($currentPage + 1) . "\"";
         } else {
             echo "disabled";
         }
@@ -143,46 +148,67 @@ function populatePagination($count, $prefix, $currentPage) {
  * @param $page int the page to display
  * @param $isAdmin boolean whether or not the logged-in user is an administrator
  */
-function populateData($conn, $prefix, $search, $sort, $page, $isAdmin) {
-    try{
-        if($prefix != ""){
+function populateData($conn, $prefix, $search, $sort, $page, $isAdmin)
+{
+    try {
+        if ($prefix != "") {
             $p = "'" . $prefix . "'";
             $var = $conn->prepare("SELECT * FROM `categories` WHERE `prefix` = $p AND `title` LIKE '%$search%' ORDER BY $sort");
             $count = $conn->query("SELECT COUNT(`title`) FROM `categories` WHERE `prefix` = $p AND `title` LIKE '%$search%'")->fetchColumn();
-        }else{
+        } else {
             $var = $conn->prepare("SELECT * FROM `categories` WHERE `title` LIKE '%$search%' ORDER BY $sort");
             $count = $conn->query("SELECT COUNT(`title`) FROM `categories` WHERE `title` LIKE '%$search%'")->fetchColumn();
         }
-        if($count == NULL){ $count = 0; }
+        if ($count == NULL) {
+            $count = 0;
+        }
         $var->execute();
 
         $c = 0;
         $p = $page != "" ? intval($page) : 1;
 
         echo "<div class='row'>";
-        while($result = $var->fetch(PDO::FETCH_ASSOC)){
-            if($c >= ($p - 1) * 18 && $c < $p * 18){
+        while ($result = $var->fetch(PDO::FETCH_ASSOC)) {
+            if ($c >= ($p - 1) * 18 && $c < $p * 18) {
 
                 //to get the number of links
                 $l = $conn->query("SELECT COUNT(link_id) FROM `links` WHERE `category_id` = '" . $result["category_id"] . "'")->fetchColumn();
-                if($l == NULL){ $l = 0; }
+                if ($l == NULL) {
+                    $l = 0;
+                }
 
                 //print out the boxes
-                echo "<div class='col-md-6'>
-								<a href='links.php?class=".$result["category_id"]."''>
-									<div class='well well-sm well-hover'>
-    								<h6 class='text-muted'>".$result["prefix"]."</h6>
-    								<h4>".$result["title"]."</h4>
-    								<p>Contains ".$l.($l > 1 ? " links" : " link").".</p>
-    								<p class='text-muted small info-text'>
-    									<span class='pull-left'>submitted by ".$result["rcs_id"]."</span>
-    									<span class='pull-right'>".$result["creation_date"]."</span>";
-                echo "<span class='clearfix'></span></p></div></a>";
+                echo "<div class='col-md-6'><a href='links.php?class=" . $result["category_id"] . "''>";
+			    echo "<div class='well well-sm well-hover'>";
 
-                if($isAdmin || $result["rcs_id"] == phpCAS::getUser()) {
+                // display the prefix for the class
+    			echo "<h6 class='text-muted'>" . $result["prefix"] . "</h6>";
+
+                // class title
+    			echo "<h4>" . $result["title"] . "</h4>";
+
+                // number of links
+    			echo "<p>Contains " . $l . ($l != 1 ? " links" : " link") . ".</p>";
+
+                // metadata
+    			echo "<p class='text-muted small info-text'>";
+
+                // submitted by
+    			echo "<span class='pull-left'>submitted by " . $result["rcs_id"] . "</span>";
+
+                // creation date
+    			echo "<span class='pull-right'>" . $result["creation_date"] . "</span>";
+
+                // fix the floats and close
+                echo "<span class='clearfix'></span>";
+                echo "</p></div></a>";
+
+                // Determine whether or not to show the edit and delete buttons
+                // Only show for admins or if the user created the class
+                if ($isAdmin || $result["rcs_id"] == phpCAS::getUser()) {
                     echo "<form method=\"post\" action='classes.php";
-                    if($prefix != ""){
-                        echo "?prefix=".$prefix;
+                    if ($prefix != "") {
+                        echo "?prefix=" . $prefix;
                     }
                     echo "' class=\"admin-panel delete-form\">";
                     echo "<button type=\"submit\" class=\"btn btn-default pull-right\" name=\"delete\" value=" . $result["category_id"] . ">Delete</button></form>";
@@ -190,14 +216,18 @@ function populateData($conn, $prefix, $search, $sort, $page, $isAdmin) {
                     echo "<button type=\"submit\" class=\"btn btn-default\" name=\"edit\" value=" . $result["category_id"] . ">Edit</button></form>";
                 }
                 echo "</div>";
-            }else if($c >= $p * 18){ break; }
+            } else if ($c >= $p * 18) {
+                break;
+            }
             ++$c;
         }
 
-        if($c == 0){
+        if ($c == 0) {
             echo "<div class='col-xs-12'>" . infoAlert("No classes. You should add one!") . "</div>";
         }
-    }catch(PDOException $e){ echo $e; }
+    } catch (PDOException $e) {
+        echo $e;
+    }
 
     populatePagination($count, $prefix, $p);
 }
@@ -220,13 +250,13 @@ $isAdmin = determineAdminStatus($conn, phpCAS::getUser());
  * Submitted actions: these are called when classes.php is loaded as an action of HTML forms from classes.php,
  * addclasses.php, and editclasses.php.
  */
-if(isset($_POST["edit"])){
+if (isset($_POST["edit"])) {
     // Update an edited class with new details
     $alertArray = executeEdit($conn, $_POST["className"], $_POST["inputCategory"], $_POST["edit"]);
-} else if(isset($_POST["user"])){
+} else if (isset($_POST["user"])) {
     // Create a newly added class with the new details
     $alertArray = executeAdd($conn, $_POST["className"], $_POST["inputCategory"], $_POST["user"]);
-} else if(isset($_POST["delete"])){
+} else if (isset($_POST["delete"])) {
     // Complete the deletion of a class
     $alertArray = executeDelete($conn, $_POST["delete"]);
 }
@@ -234,7 +264,7 @@ if(isset($_POST["edit"])){
 /**
  * Uses the responses of executeEdit, executeAdd, and executeDelete to generate appropriate alerts as necessary.
  */
-if(isset($alertArray)) {
+if (isset($alertArray)) {
     $alertType = $alertArray[0];
     $alertMessage = $alertArray[1];
 } else {
